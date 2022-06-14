9fc6f2c92cdca8ba4740bdc57546c3aae00b289d
Matteo Lucarno <lucarnosky@gmail.com>
2022-06-14 20:20:48 +0200

    Fix Auth request prototype

--- middleware/checkToken.js
+++ middleware/checkToken.js
@@ -1,37 +1,42 @@
-const jwt = require('jsonwebtoken');
-const userCtrl = require('../controllers/users');
-const { Pool } = require('pg');
+const jwt = require("jsonwebtoken");
+const userCtrl = require("../controllers/users");
+const dotenv = require("dotenv");
+const { Client } = require("pg");
+const { Pool } = require('pg')
 
+dotenv.config();
 exports.verifyToken = async (req, res, next) => {
-    try {
-        const token = req.headers["x-access-token"];
-        if (!token) return res.status(403).json({
-            message: "No token provided"
-        })
-        const decoded = jwt.verify(token, 'secretKey')
-        req.userId = decoded.id;
-        const res = await Pool.query(
-           "SELECT * FROM users WHERE userid = "+decoded.id
-        );
-        console.log(res.rows[0]);
-        /*.then
-        con.query(sql, [req.userId], function (err, result) {
-            console.log("Test 2");
-            if (err)
-                res.json({ status: "false", message: "Error occured" });
-            user = result.rows[0];
-            console.log (user);
-            if (!user) return res.status(404).json({
-                message: "No user found"
-            })
-            console.log("E si va");
-            next();
-        });
-        console.log("Er!");*/
-    } catch (error) {
-        return res.status(401).json({
-            message: "Unauthorized"
-        })
-    }
+  try {
+    const token = req.headers["x-access-token"];
+    if (!token)
+      return res.status(403).json({
+        message: "No token provided",
+      });
+    const decoded = jwt.verify(token, "secretKey");
 
-}
+    req.userId = decoded.id;
+    const pool = new Pool({
+        connectionString: process.env.DB_CONNECTION_STRING,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    })
+    const client = await pool.connect();
+    const result = await client.query({
+      rowMode: "array",
+      text: "SELECT userid FROM users WHERE userid = "+req.userId,
+    });
+    await client.end();
+    if(result.rows.length == 0){
+        return res.status(403).json({
+            message: "Invalid token",
+        });
+    }else{
+        next();
+    }
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+};
